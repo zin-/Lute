1bedebf1c260154f0a3315170089b7496b84503a
zin- <noyu718@gmail.com>
2020-05-06 02:26:41 +0900

    add dart library example

---
+++ example/lute_example.dart
@@ -0,0 +1,6 @@
+import 'package:lute/lute.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/lute.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library lute;
+
+export 'src/lute_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
